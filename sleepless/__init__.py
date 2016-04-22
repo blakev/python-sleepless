dd7054246a3063ee3da4011a32ccecce25abdf30
Blake VandeMerwe <blake.vandemerwe@vivint.com>
2016-04-22 16:19:42 -0600

    first

---
+++ sleepless/__init__.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# >>
+#     python-sleepless, 2016
+# <<
+"""
+    sleepless
+    ~~~~~~~~~
+
+    Sleepless is a REST API consumer to allow pragmatic access to other's
+    APIs without worrying about all the boilerplate.
+
+    :copyright: (c) 2016, Blake VandeMerwe
+    :license:   GPLv3, see LICENSE for more details.
+
+"""
+__docformat__ = 'restructuredtext en'
+__version__ = '0.1.dev'
+
+__all__ = [
+
+]
